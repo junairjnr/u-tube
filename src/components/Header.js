31cb517762779903b6c6a3c4a7cb70bd54972d4c
Junair <junairjunu869@gmail.com>
2023-11-17 14:29:49 +0530

    initial

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Header</h1>
+      <MenuIcon />
+    </div>
+  )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC0RtrlvoJZ4jB470BGnVM1yARENCrDNHc",
+    authDomain: "fir-ed7ce.firebaseapp.com",
+    projectId: "fir-ed7ce",
+    storageBucket: "fir-ed7ce.appspot.com",
+    messagingSenderId: "1038653736766",
+    appId: "1:1038653736766:web:6454763f4f8d98cebe4e6f",
+    measurementId: "G-PYBDK7K0X4"
+  };
